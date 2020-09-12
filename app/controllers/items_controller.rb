5c6316d037ce1d49a9c63a2b3bf32f2a1539343f
seidai-nakagawa <67330787+seidai-nakagawa@users.noreply.github.com>
2020-09-12 15:16:24 +0900

    class訂正&ヘッダーフッダー削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    render layout: false
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :items,only: [:new, :create] do
+  end
 end
