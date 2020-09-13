cec21b5ee16cfafaca27b5f457b630862b783561
gelsy-0517 <toku.shigeru.5.17@gmail.com>
2020-09-13 19:03:00 +0900

    ユーザーマイページのフロント作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: :show
+  resources :items,only: [:new, :create] do
+  resources :users,only: [:show,] do
+end
+end
 end
