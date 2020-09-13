39a9153e454e1dce2c2fb5e70283165ea0f194ff
koyashika <u_wh.sunnnny.xx@softbank.ne.jp>
2020-09-13 16:14:56 +0900

    商品詳細ページのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
-  get  "item/purchase"  => "items#purchase"
+  get "item/purchase"  => "items#purchase"
+  get "item/show" => "items#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
