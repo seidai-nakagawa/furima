adb0281bd6957b6ab8da4f7a6dfe108100a95767
gelsy-0517 <toku.shigeru.5.17@gmail.com>
2020-09-16 21:33:08 +0900

    6, 8行目でusersの定義が重複している。つまり8~9行目不要なので削除

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: :show
   resources :items,only: [:new, :create] do
-  resources :users,only: [:show,] do
-end
 end
 end
