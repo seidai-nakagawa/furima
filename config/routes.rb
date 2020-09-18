9063f1e354eb64e06e045e873029e519e55ed09c
koyashika <u_wh.sunnnny.xx@softbank.ne.jp>
2020-09-19 01:00:23 +0900

    routingをresourcesベースに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'items#index'
-  get "item/purchase"  => "items#purchase"
-  get "item/show" => "items#show"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources "items", only: [:show] do
+    collection do
+      get 'purchase'
+    end
+  end
 end
