e51c75ba559272611c5f024520ada2e1a9c984fc
Youmisawa <66550111+Youmisawa@users.noreply.github.com>
2020-09-19 17:04:30 +0900

    deviseのストロングパラメーターにパラメーター追加
    configure_permitted_parametersメソッドを使用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :basic_auth, if: :production?
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   private
 
   def basic_auth
@@ -10,7 +11,12 @@ class ApplicationController < ActionController::Base
       password == Rails.application.credentials[:basic_auth][:pass]
     end
   end
+
   def production?
     Rails.env.production?
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_furigana, :first_name_furigana, :birth_day, :family_name_to_deliver, :first_name_to_deliver, :family_name_to_deliver_furigana, :first_name_to_deliver_furigana, :postal_code, :prefecture_id, :city_name, :city_address, :building, :phone_number])
   end
 end
