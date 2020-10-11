5037e26a1165a69327a83327f1cec4a4576d9e94
seidai-nakagawa <67330787+seidai-nakagawa@users.noreply.github.com>
2020-10-11 15:13:59 +0900

    brandテーブル(及びマイグレーションファイル)の削除。

--- db/migrate/20200922061823_create_brands.rb
+++
@@ -1,9 +0,0 @@
-class CreateBrands < ActiveRecord::Migration[6.0]
-  def change
-    create_table :brands do |t|
-      ## Database authenticatable
-      t.string :name,                               null: false
-      t.timestamps
-    end
-  end
-end
