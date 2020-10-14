49c9f92b1ff2251a2d7743a145799bbf35544096
Youmisawa <66550111+Youmisawa@users.noreply.github.com>
2020-10-14 16:08:34 +0900

    categoryの前準備
    items_controller.rb+category.rb+routes.rb+seeds.rb+gem追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :category_parent_array, only: [:new, :create, :edit, :update]
   def index
   end
 
@@ -25,15 +26,32 @@ class ItemsController < ApplicationController
   def purchase
   end
 
+  # 親カテゴリが選択された後に動くアクション
+  def get_category_children
+    # 選択された親カテゴリに紐づく子カテゴリの配列を取得
+    @category_children = Category.find("#{params[:parent_id]}").children
+  end
+
+  # 子カテゴリが選択された後に動くアクション
+  def get_category_grandchildren
+    # 選択された子カテゴリに紐づく孫カテゴリの配列を取得
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
+
 
   private
   
   def item_params
     params.require(:item).permit(:name, :price, :explanation, :brand, :size_id, :state_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, item_images_attributes: [:url]).merge(user_id: current_user.id, category_id: "1")
   end
+
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     end
+  end
+
+  def category_parent_array
+    @category_parent_array = Category.where(ancestry: nil)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   root 'items#index'
   resources "items", only: [:new, :create] do
   end
+  resources :items do
+    collection do
+      get 'get_category_children', defaults: { fomat: 'json'}
+      get 'get_category_grandchildren', defaults: { fomat: 'json'}
+    end
+  end
   resources "items", only: [:show] do
     collection do
       get 'purchase'
