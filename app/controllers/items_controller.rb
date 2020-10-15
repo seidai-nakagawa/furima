62ae3a3f83e1c30d8a3127eb2df37a6dabe2f312
seidai-nakagawa <67330787+seidai-nakagawa@users.noreply.github.com>
2020-10-15 17:15:36 +0900

    出品するボタンで(edit→updateへ)遷移。フラッシュメッセージ(notice)追加。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,10 +37,19 @@ class ItemsController < ApplicationController
     # end
   end
 
+def update
+  @item = Item.find(params[:id])
+  if @item.update(item_params)
+    redirect_to root_path, notice: '出品内容を更新しました'
+  else
+    render :edit
+  end
+end
+
   private
   
   def item_params
-    params.require(:item).permit(:name, :price, :explanation, :brand, :size_id, :state_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, item_images_attributes: [:url]).merge(user_id: current_user.id, category_id: "1")
+    params.require(:item).permit(:name, :price, :explanation, :brand, :size_id, :state_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, item_images_attributes: [:url, :_destroy, :id]).merge(user_id: current_user.id, category_id: "1")
   end
   def move_to_index
     unless user_signed_in?
