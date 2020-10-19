1444d539f0364a65528f25db3da6d3d20cd365ce
koyashika <u_wh.sunnnny.xx@softbank.ne.jp>
2020-10-19 09:33:37 +0900

    cardsコントローラーにnew・createアクションを記載

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,24 @@
+class CardsController < ApplicationController
+  require "payjp" 
+
+  def new
+  end
+
+  def create
+    Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+    if params["payjp_token"].blank?
+      redirect_to action: "new", alert: "クレジットカードを登録できませんでした。"
+    else
+      customer = Payjp::Customer.create(
+        email: current_user.email,
+        card: params["payjp_token"],
+        metadata: {user_id: current_user.id}
+      )
+      @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+      else
+        redirect_to action: "create"
+      end
+    end
+  end
+end
