b91ff9c8cc4b1fabec64ad4ca7f42ac7a265706f
koyashika <u_wh.sunnnny.xx@softbank.ne.jp>
2020-10-24 16:15:17 +0900

    ImageUploederにcarrierwaveの設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -44,4 +44,20 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w(png jpg)
+  end
+
+  def filename
+    original_filename if original_filename
+  end
 end
