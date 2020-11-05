13f331825c326c90b08af9166b93f457e5d4121d
Youmisawa <66550111+Youmisawa@users.noreply.github.com>
2020-11-06 01:30:43 +0900

    スペルミス

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :items do
     collection do
-      get 'get_category_children', defaults: { fomat: 'json'}
-      get 'get_category_grandchildren', defaults: { fomat: 'json'}
+      get 'get_category_children', defaults: { format: 'json'}
+      get 'get_category_grandchildren', defaults: { format: 'json'}
     end
     member do
       get 'get_category_children', defaults: { format: 'json'}
